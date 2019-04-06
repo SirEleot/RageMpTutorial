85029621330406f536c26c5d75292dc5b688e4e1
SirEleot <sireleot@gmail.com>
2019-04-06 17:47:21 +0300

    events

---
+++ Client/ClientTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RAGE;
+
+namespace Tuter
+{
+    public class ClientTest : Events.Script
+    {
+        public ClientTest()
+        {
+            Events.OnPlayerChat += OnPlayerChat;
+            Events.Add("cl_TestEvent", OnTestEvent);
+        }
+
+        private void OnTestEvent(object[] args)
+        {
+            string Message = (string) args[0];
+            Chat.Output(Message);
+        }
+
+        private void OnPlayerChat(string text, Events.CancelEventArgs cancel)
+        {
+            Events.CallRemote("srv_TestEvent", text);
+        }
+    }
+}
---
+++ Server/ServerTest.cs
@@ -0,0 +1,21 @@
+﻿using GTANetworkAPI;
+using System;
+
+namespace Tuter
+{
+    public class ServerTest : Script
+    {
+       [ServerEvent(Event.PlayerConnected)]
+       public void OnPlayerConnected(Client client)
+       {
+            NAPI.Util.ConsoleOutput($"Player name: {client.Name}");
+            client.TriggerEvent("cl_TestEvent", "Remote event is worked");
+       }
+
+        [RemoteEvent("srv_TestEvent")]
+        public void OnTestEvent(Client clien, string message)
+        {
+            NAPI.Util.ConsoleOutput($"Test message: {message}");
+        }
+    }
+}
